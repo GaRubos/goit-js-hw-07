7212b2ffe7aa19d6eef64e10facd135487f7b88e
Ruslan Garmash <garmash.rb@gmail.com>
2023-02-14 20:15:32 +0200

    add task 1 and task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+function createGalleryCollection(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+           <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+            </a>
+        </li>`;
+    })
+    .join("");
+}
+const container = document.querySelector(".gallery");
+const imgItem = createGalleryCollection(galleryItems);
+container.insertAdjacentHTML("beforeend", imgItem);
+container.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  openOriginalImgModal(event);
+}
+
+function openOriginalImgModal(event) {
+  const imgEl = basicLightbox.create(
+    `<img width="1280" height="968" alt="${event.target.alt}" src= "${event.target.dataset.source}" ></img>`
+  );
+  imgEl.show();
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      imgEl.close();
+    }
+  });
+}
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+function createGalleryCollection(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+           <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+            </a>
+        </li>`;
+    })
+    .join("");
+}
+const container = document.querySelector(".gallery");
+const img = createGalleryCollection(galleryItems);
+container.insertAdjacentHTML("beforeend", img);
+container.addEventListener("click", nameImages);
+
+function nameImages(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+  }
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  scrollZoom: true,
+  captionDelay: 250,
+  captionsData: "alt",
+  doubleTapZoom: 2,
+});
 
 console.log(galleryItems);
